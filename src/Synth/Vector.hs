d4720224bd086b0b90fcf60c1bb8c0dfb8399bc4
Rob Rix <rob.rix@me.com>
2018-11-22 03:49:15 -0500

    Define a vector datatype.

--- src/Synth/Vector.hs
+++ src/Synth/Vector.hs
@@ -1,1 +1,9 @@
+{-# LANGUAGE DataKinds, GADTs #-}
 module Synth.Vector where
+
+import Synth.Shape
+
+data V s a where
+  V0 :: V s a
+  V1 :: a -> V 'S1 a
+  VB :: V s1 a -> V s2 a -> V ('SB s1 s2) a
