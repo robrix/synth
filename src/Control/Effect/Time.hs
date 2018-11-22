1ed9bf729fc9c882fcf7443641df51f8d890cd53
Rob Rix <rob.rix@me.com>
2018-11-22 18:18:19 -0500

    Define an HFunctor instance for Time.

--- src/Control/Effect/Time.hs
+++ src/Control/Effect/Time.hs
@@ -1,6 +1,12 @@
 {-# LANGUAGE DeriveFunctor, KindSignatures #-}
 module Control.Effect.Time where
 
+import Control.Effect.Carrier
+import Data.Coerce
+
 data Time (m :: * -> *) k
   = Time (Double -> k)
   deriving (Functor)
+
+instance HFunctor Time where
+  hmap _ = coerce
