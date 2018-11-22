4d0199e6c097d869e6b5dd3d03a4cbfdb70e774a
Rob Rix <rob.rix@me.com>
2018-11-22 03:50:13 -0500

    Derive some instances for vectors.

--- src/Synth/Vector.hs
+++ src/Synth/Vector.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DataKinds, GADTs #-}
+{-# LANGUAGE DataKinds, GADTs, StandaloneDeriving #-}
 module Synth.Vector where
 
 import Synth.Shape
@@ -7,3 +7,7 @@ data V s a where
   V0 :: V s a
   V1 :: a -> V 'S1 a
   VB :: V s1 a -> V s2 a -> V ('SB s1 s2) a
+
+deriving instance Eq   a => Eq   (V s a)
+deriving instance Ord  a => Ord  (V s a)
+deriving instance Show a => Show (V s a)
