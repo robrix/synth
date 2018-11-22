b403b6f0464f982bc5f43fdff77c006e9d33c61a
Rob Rix <rob.rix@me.com>
2018-11-22 03:39:26 -0500

    Compute the maximum depth of a shape.

--- src/Synth/Shape.hs
+++ src/Synth/Shape.hs
@@ -2,3 +2,8 @@ module Synth.Shape where
 
 data S = S0 | S1 | SB S S
   deriving (Eq, Ord, Show)
+
+smax :: Num a => S -> a
+smax S0         = 0
+smax S1         = 1
+smax (SB s1 s2) = smax s1 + smax s2
