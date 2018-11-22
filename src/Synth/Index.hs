7d1f2ffc28fb1e9e32caae2191cc2aeb14ccdb40
Rob Rix <rob.rix@me.com>
2018-11-22 03:43:14 -0500

    Define an Ord instance for indices.

--- src/Synth/Index.hs
+++ src/Synth/Index.hs
@@ -9,3 +9,10 @@ data I a where
   IR :: I i2 -> I ('SB i1 i2)
 
 deriving instance Eq (I a)
+
+instance Ord (I s) where
+  compare I1      I1      = EQ
+  compare (IL i1) (IL i2) = compare i1 i2
+  compare (IL _)  _       = LT
+  compare _       (IL _)  = GT
+  compare (IR i1) (IR i2) = compare i1 i2
