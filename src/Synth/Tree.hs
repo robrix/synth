309dfcbdc83015a5274afc19ae99db9d17615cb4
Rob Rix <rob.rix@me.com>
2018-11-22 04:02:44 -0500

    Define a function mapping lists into binary trees.

--- src/Synth/Tree.hs
+++ src/Synth/Tree.hs
@@ -3,3 +3,9 @@ module Synth.Tree where
 
 data B a = B0 | B1 a | BB (B a) (B a)
   deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
+
+fromList :: [a] -> B a
+fromList []  = B0
+fromList [x] = B1 x
+fromList xs  = BB (fromList ys) (fromList zs)
+  where (ys, zs) = splitAt (length xs `div` 2) xs
