126f4b2f5cd68086117ff84516a0452611b2b3ab
Rob Rix <rob.rix@me.com>
2018-11-22 03:43:08 -0500

    Derive an Eq instance for indices.

--- src/Synth/Index.hs
+++ src/Synth/Index.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DataKinds, GADTs #-}
+{-# LANGUAGE DataKinds, GADTs, StandaloneDeriving #-}
 module Synth.Index where
 
 import Synth.Shape
@@ -7,3 +7,5 @@ data I a where
   I1 :: I 'S1
   IL :: I i1 -> I ('SB i1 i2)
   IR :: I i2 -> I ('SB i1 i2)
+
+deriving instance Eq (I a)
