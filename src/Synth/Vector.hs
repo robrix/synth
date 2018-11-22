e8675e67b2884c102966bf96d1dab3689c9e06ba
Rob Rix <rob.rix@me.com>
2018-11-22 03:52:19 -0500

    Define a Semigroup instance for vectors.

--- src/Synth/Vector.hs
+++ src/Synth/Vector.hs
@@ -14,3 +14,9 @@ deriving instance Show a => Show (V s a)
 deriving instance Foldable (V s)
 deriving instance Functor (V s)
 deriving instance Traversable (V s)
+
+instance Semigroup a => Semigroup (V s a) where
+  V0       <> a2       = a2
+  a1       <> V0       = a1
+  V1 a1    <> V1 a2    = V1 (a1 <> a2)
+  VB a1 b1 <> VB a2 b2 = VB (a1 <> a2) (b1 <> b2)
