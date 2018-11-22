147aa858b0ae799652506f817c15499ac18ab0fc
Rob Rix <rob.rix@me.com>
2018-11-22 18:28:18 -0500

    Define a handler for Time.

--- src/Control/Effect/Time.hs
+++ src/Control/Effect/Time.hs
@@ -1,8 +1,12 @@
-{-# LANGUAGE DeriveFunctor, KindSignatures #-}
+{-# LANGUAGE DeriveFunctor, FlexibleInstances, KindSignatures, MultiParamTypeClasses, TypeOperators, UndecidableInstances #-}
 module Control.Effect.Time where
 
 import Control.Effect.Carrier
+import Control.Effect.Internal
+import Control.Effect.Sum
+import Control.Monad.IO.Class
 import Data.Coerce
+import Data.Time.Clock.System
 
 data Time (m :: * -> *) k
   = Time (Double -> k)
@@ -13,3 +17,16 @@ instance HFunctor Time where
 
 instance Effect Time where
   handle state handler = coerce . fmap (handler . (<$ state))
+
+
+runTime :: (Carrier sig m, MonadIO m) => Eff (TimeC m) a -> m a
+runTime = runTimeC . interpret
+
+newtype TimeC m a = TimeC { runTimeC :: m a }
+
+instance (Carrier sig m, MonadIO m) => Carrier (Time :+: sig) (TimeC m) where
+  ret = TimeC . ret
+  eff = TimeC . handleSum (eff . handleCoercible) (\ (Time k) -> liftIO getSystemTime >>= runTimeC . k . systemToFracTime)
+
+systemToFracTime :: Fractional a => SystemTime -> a
+systemToFracTime (MkSystemTime s ns) = (fromIntegral s) + (fromIntegral ns) * 1E-9
